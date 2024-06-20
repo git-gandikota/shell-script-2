7a20a20be858d0fa550a128f6f9acf718e3a7ade
git-gandikota <ganidkota.fou@gamil.com>
2024-06-20 11:23:21 +0530

    Calling one script from other shell script

--- 01-current-shell-script.sh
+++ 01-current-shell-script.sh
@@ -5,6 +5,6 @@ COURSE="DevOps from Current Script"
 echo "Before calling Other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-D:\devops\daws78-s\dev\repos\shell-script-2\02-other-shell-script.sh
+./02-other-shell-script.sh
 
 echo "After calling Other script, course: $COURSE"
