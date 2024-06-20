880920d1d722510be425099cc4e26b43192edf3a
git-gandikota <ganidkota.fou@gamil.com>
2024-06-20 13:00:55 +0530

    delete logs functions

--- 08-delete-old-logs.sh
+++ 08-delete-old-logs.sh
@@ -11,6 +11,6 @@ if [ -d $SOURCE_DIRECTORY ]
 then
     echo -e "$G Source directory exists $N"
 else
-    echo -e "$R Please make sure $SOURCE exists $N"
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
     exit 1
 fi
