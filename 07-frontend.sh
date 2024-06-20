0a33ab7eca1f36ba5fac79449672557fb96f0d78
git-gandikota <ganidkota.fou@gamil.com>
2024-06-20 12:43:13 +0530

    Script with out validate function

---
+++ 07-frontend.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+dnf install nginx -y &>>$LOGFILE
+
+systemctl enable nginx &>>$LOGFILE
+
+systemctl start nginx &>>$LOGFILE
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+
+cd /usr/share/nginx/html &>>$LOGFILE
+
+
+cp /home/ec2-user/practice-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+
+systemctl restart nginx &>>$LOGFILE
